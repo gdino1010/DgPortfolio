e20cd1fae714ad7b63c3b9dbbfed7a5b08fe5dad
David <g.dino1010@gmail.com>
2026-01-07 22:57:16 -0800

    Version 16 Contact card functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,64 @@
+import { useState } from "react";
+
 export default function Contact() {
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+            formData.append("access_key", "82bf8034-108b-4a57-8439-bc7c38cfbc38");
+
+            try {
+              const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData,
+              });
+
+              const data = await response.json();
+
+              if (data.success) {
+                setResult("Message sent successfully!");
+                event.target.reset();
+              } else {
+                setResult("Something went wrong. Please try again.");
+              }
+            } catch (error) {
+              setResult("Network error. Please try again.");
+            }
+          };
+
   return (
     <div className="container contact">
       <div className="card">
         <h2>Contact Me!</h2>
 
-        <form className="form" onSubmit={(e) => e.preventDefault()}>
+        <form className="form" onSubmit={onSubmit}>
           <label>
             <span>Name</span>
-            <input type="text" name="name" autoComplete="name" />
+            <input type="text" name="name" autoComplete="name" required/>
           </label>
 
           <label>
             <span>Email</span>
-            <input type="email" name="email" autoComplete="email" />
+            <input type="email" name="email" autoComplete="email" required/>
           </label>
 
           <label className="span-2">
             <span>Subject</span>
-            <input type="text" name="subject" />
+            <input type="text" name="subject" required/>
           </label>
 
           <label className="span-2">
             <span>Message</span>
-            <textarea name="message" rows="6" />
+            <textarea name="message" rows="6" required/>
           </label>
 
           <div className="actions span-2">
             <button className="btn" type="submit">Enter</button>
           </div>
+
+            {result && <p className="form-result span-2">{result}</p>}
         </form>
       </div>
     </div>
